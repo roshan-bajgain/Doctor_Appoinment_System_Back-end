d0bf4b59941680cbd65b202a5cbc012ab40d194b
roshan-bajgain <roshan.bazgain@gmail.com>
2025-07-12 15:00:00 +0545

    Add logic for availability

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const doctorSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     image: { type: String, required: true },
     speciality: { type: String, required: true },
