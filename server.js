8f79c05b750cec835b638666a6e781f8162f9c5b
roshan-bajgain <roshan.bazgain@gmail.com>
2025-07-15 15:00:00 +0545

    created doctor model

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./config/mongodb.js";  // Import the connectDB function
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";  
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
+connectCloudinary(); 
 //middleware
 app.use(express.json());
 app.use(cors());
