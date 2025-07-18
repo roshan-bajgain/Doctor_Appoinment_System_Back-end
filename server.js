36c3187b0b1d892c3e25309baf983fa9dd18299b
roshan-bajgain <roshan.bazgain@gmail.com>
2025-07-18 15:00:00 +0545

    Database connected with mongodb

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";  // Import the connectDB function
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 //middleware
 app.use(express.json());
 app.use(cors());
 
 //api endpoints
-
 app.get("/", (req, res) => {
   res.send(
     "Welcome to the Doctor Appointment System API roshan bajgin from jhapa"
