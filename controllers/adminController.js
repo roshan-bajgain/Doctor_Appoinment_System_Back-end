63520fd6df484e7eee6e720759f7b510dbfd0c5c
roshan-bajgain <roshan.bazgain@gmail.com>
2025-07-28 22:32:20 +0545

    Add multer for file upload

---
+++ controllers/adminController.js
@@ -0,0 +1,8 @@
+//Api for adding doctor
+
+const addDoctor = async (req,res) => {
+  try {
+    const {name, email, password, image, speciality, degree, experience, about, fees, address} = req.body;
+  } catch (error){
+
+  }}
