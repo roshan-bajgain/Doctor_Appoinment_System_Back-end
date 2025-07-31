269e9b6c214921b7f8e6f4da1e17418f611ee29b
roshan-bajgain <roshan.bazgain@gmail.com>
2025-07-31 11:47:24 +0545

    Add logic for User registration

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";  
+import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
-connectCloudinary(); 
+connectCloudinary();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 //api endpoints
-app.use("/api/admin",adminRouter) //localhost:5000/api/admin/add-doctor
+app.use("/api/admin", adminRouter); //localhost:5000/api/admin/add-doctor
 app.use("/api/doctor", doctorRouter); //localhost:5000/api/doctor/list
-
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send(
