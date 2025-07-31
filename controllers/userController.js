acec438457c615420455e203ac1e3fe9201a0b4e
roshan-bajgain <roshan.bazgain@gmail.com>
2025-07-31 12:50:25 +0545

    Add logic for login

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,4 +41,28 @@ const registerUser = async (req, res) => {
   }
 };
 
-export {registerUser}
+const loginUser = async (req,res) => {
+  try {
+    const {email, password} = req.body
+    const user = await userModel.findOne({email})
+    if(!user) {
+      res.json({success:false, message: 'User doesnto exit'})
+    }
+
+    const  isMatch = await bcrypt.compare(password, user.password)
+
+    if(isMatch) {
+      const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+      res.json({success:true, token})
+    } else {
+      res.json({success:false, message:"Invalid credential"})
+    }
+
+
+  } catch (error) {
+     console.log(error);
+     res.json({ success: false, message: error.message });
+  }
+}
+
+export {registerUser, loginUser}
